36b8a3e03dff14fe767a9c45b7aea7e20f3f94aa
javierrm92 <160373709+javierrm92@users.noreply.github.com>
2024-05-10 00:40:27 -0600

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "examplestorageaccount"
+    storage_account_name = "examplestorageaccount109"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     sas_token            = var.sas_token
